bd134db57ce58c8c54ae203a4cbfb0612c77d9fc
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-06-14 15:05:00 +0000

    https://github.com/4GeeksAcademy/New_Ricardo_randomcardgen.git

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".top-suite").innerHTML = random_top_suite;
+  document.querySelector(".number").innerHTML = random_number();
+  document.querySelector(".bottom-suite").innerHTML = random_top_suite;
+};
+
+let random_suite = () => {
+  let suite = ["♠", "♥", "♣", "♦"];
+  let random_number = Math.floor(Math.random() * suite.length);
+  return suite[random_number];
+};
+
+let random_top_suite = random_suite();
+let random_number = () => {
+  let number = ["A", "K", "Q", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let random_number = Math.floor(Math.random() * number.length);
+  return number[random_number];
 };
